96e4313dbcad9a6ca3cb9ccd006f435b4c73eca5
Dmitry-Mihajlov <dimonmihajlov10@gmail.om>
2024-01-15 04:40:25 +0500

    добавляется текст в практику

--- main.py
+++ main.py
@@ -7,24 +7,36 @@ from kivymd.uix.textfield import MDTextField
 
 Builder.load_file('main_screen.kv')
 
+data = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "Sed blandit libero volutpat sed cras ornare arcu. Nisl vel pretium "
+    "lectus quam id leo in. Tincidunt arcu non sodales neque sodales ut etiam.",
+    "Elit scelerisque mauris pellentesque pulvinar pellentesque habitant. "
+    "Nisl rhoncus mattis rhoncus urna neque. Orci nulla pellentesque "
+    "dignissim enim. Ac auctor augue mauris augue neque gravida in fermentum. "
+    "Lacus suspendisse faucibus interdum posuere."
+
+]
+
+class CopyLabel(MDLabel):
+    # Преобразование элементов для текста
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.allow_selection = True
+        self.adaptive_height = True
+        self.theme_text_color = "Custom"
+        self.text_color = self.theme_cls.text_color
+
 
 class MainScreen(Screen):
     pass
 
 
 class MainApp(MDApp):
-    def on_tab_screen_2(self):
-        screen = Builder.load_file('test_screen.kv')
-        print('-----------')
-        for i in range(5):
-            screen.ids.box.add_widget(
-                MDLabel(
-                    text='gggggg',
-                    halign='center',
-                    adaptive_height=True
-                )
-            )
-        return screen
+    def on_start(self):
+        for text in data:
+            copy_label = CopyLabel(text=text)
+            self.root.ids.box.add_widget(copy_label)
 
     def build(self):
         Window.size = (300, 500)
--- test/main_test.py
+++ test/main_test.py
@@ -1,34 +1,66 @@
-from kivy.lang import Builder
+from kivy.core.clipboard import Clipboard
+from kivy.lang.builder import Builder
+from kivy.metrics import dp
 
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
-from kivymd.font_definitions import theme_font_styles
+from kivymd.uix.menu import MDDropdownMenu
+from kivymd.toast import toast
 
 KV = '''
-MDScrollView:
+MDBoxLayout:
+    orientation: "vertical"
+    spacing: "12dp"
+    padding: "24dp"
 
-    BoxLayout:
-        orientation: "vertical"
-        MDLabel:
-            text: "1 style"
-            halign: "center"
+    MDScrollView:
+
+        MDBoxLayout:
+            id: box
+            orientation: "vertical"
+            padding: "24dp"
+            spacing: "12dp"
             adaptive_height: True
-        MDLabel:
-            text: "2 style"
-            halign: "center"
-            adaptive_height: True
-        MDLabel:
-            text: "3 style"
-            halign: "center"
-            adaptive_height: True
+
+    MDTextField:
+        max_height: "200dp"
+        mode: "fill"
+        multiline: True
+
+    MDWidget:
 '''
 
+data = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "Sed blandit libero volutpat sed cras ornare arcu. Nisl vel pretium "
+    "lectus quam id leo in. Tincidunt arcu non sodales neque sodales ut etiam.",
+    "Elit scelerisque mauris pellentesque pulvinar pellentesque habitant. "
+    "Nisl rhoncus mattis rhoncus urna neque. Orci nulla pellentesque "
+    "dignissim enim. Ac auctor augue mauris augue neque gravida in fermentum. "
+    "Lacus suspendisse faucibus interdum posuere."
 
-class Test(MDApp):
+]
+
+
+class CopyLabel(MDLabel):
+    # Преобразование элементов для текста
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.allow_selection = True
+        self.adaptive_height = True
+        self.theme_text_color = "Custom"
+        self.text_color = self.theme_cls.text_color
+
+
+class Example(MDApp):
+
     def build(self):
-        screen = Builder.load_string(KV)
+        return Builder.load_string(KV)
 
-        return screen
+    def on_start(self):
+        for text in data:
+            copy_label = CopyLabel(text=text)
+            self.root.ids.box.add_widget(copy_label)
 
 
-Test().run()
+Example().run()
