4a315d8bed622bee19a0f7c5e4fce29be5a08c04
Dmitry-Mihajlov <dimonmihajlov10@gmail.om>
2024-01-21 12:52:09 +0500

    v4.5

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ from kivymd.uix.screen import Screen
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.textfield import MDTextField
-from data import data, data_answer
-import random
+from Russion_language_application.data import data, data_answer
 
+Builder.load_file('D:\Programming\PyCharm\Russion_language_application\main_screen.kv')
 data_button = []
-data_count_app = 0
 
 
 class CopyLabel_1(MDLabel):
@@ -39,64 +38,17 @@ class MainScreen(Screen):
 
 class MainApp(MDApp):
 
-    def start_app(self):
-        kv = '''
-    <MainScreen>:
+    def on_start(self):
+        global data_button
+        data_button = []
 
-    BoxLayout:
-        orientation: 'vertical'
-        MDBottomNavigation:
-            #panel_color: "#eeeaea"
-            text_color_active: "lightgrey"
-            text_color_normal: 1, 1, 1, 1
-            text_color_active: 0, 0, 0, 1
+        self.data_answer_flag = [1 for i in range(10)]
+        self.data_count_app = 0
 
-            MDBottomNavigationItem:
-                name: 'screen 1'
-                text: 'Практикум'
-                icon: 'pencil'
-
-                MDBoxLayout:
-                    orientation: "vertical"
-                    MDBoxLayout:
-                        ScrollView:
-                            MDBoxLayout:
-                                id: box
-                                orientation: "vertical"
-                                padding: "10dp"
-                                spacing: "10dp"
-                                adaptive_height: True
-                                MDLabel:
-                                    adaptive_height: True
-                                    font_style: 'Button'
-                                    text: '9 задание'
-                                    halign: 'center'
-
-            MDBottomNavigationItem:
-                name: 'screen 2'
-                text: 'Теория'
-                icon: 'book'
-
-                MDLabel:
-                    text: 'Сакшен Сакшен Сакшен Сакс'
-                    halign: "center"
-
-        '''
-        global data_answer_flag, data_count_app
-        data_count_app = 0
-        data_answer_flag = [1 for i in range(10)]
-
-        Builder.load_string(kv)
-        print('перезапуск')
-        MainApp().start_func(kv)
-
-
-    def start_func(self, kv):
-        data_app = random.choices(data, k=1)
-        for i in range(len(data_app)):
+        for i in range(len(data)):
             text_1 = data[i][0]
             text_2 = data[i][1]
-            locale_1 = f'''
+            self.locale_1 = f'''
 MDTextField:
     id: {i}
     hint_text: "Ответ"
@@ -104,28 +56,27 @@ MDTextField:
     font_size: '10sp'
     helper_text_mode: "on_focus"
 '''
-            locale_2 = f'''  
+            self.locale_2 = f'''  
 MDRectangleFlatIconButton:
     icon: 'key'
     text: 'Проверить ответ'
     theme_text_color: 'Hint'
     on_release: app.show_data({i})
-    
 '''
 
-            copy_label_1 = CopyLabel_1(text=text_1)
-            copy_label_2 = CopyLabel_2(text=text_2)
-            self.root.ids.box.add_widget(copy_label_1)
-            self.root.ids.box.add_widget(copy_label_2)
+            self.copy_label_1 = CopyLabel_1(text=text_1)
+            self.copy_label_2 = CopyLabel_2(text=text_2)
+            self.root.ids.box.add_widget(self.copy_label_1)
+            self.root.ids.box.add_widget(self.copy_label_2)
 
-            data_1 = Builder.load_string(locale_1)
-            data_button.append(data_1)
-            data_2 = Builder.load_string(locale_2)
+            self.data_1 = Builder.load_string(self.locale_1)
+            data_button.append(self.data_1)
+            self.data_2 = Builder.load_string(self.locale_2)
 
-            self.root.ids.box.add_widget(data_1)
-            self.root.ids.box.add_widget(data_2)
+            self.root.ids.box.add_widget(self.data_1)
+            self.root.ids.box.add_widget(self.data_2)
 
-            if i == len(data_app) - 1:
+            if i == len(data) - 1:
                 self.root.ids.box.add_widget(Builder.load_string(
                     '''
 MDRoundFlatButton:
@@ -135,15 +86,17 @@ MDRoundFlatButton:
     pos_hint: {"center_x": .5}
 '''
                 ))
+        id_old = i
 
     def result(self):
         def func():
+            self.root.ids.box.clear_widgets()
             dialog.dismiss()
             self.on_start()
 
-        if data_count_app < len(data) / 5:
+        if self.data_count_app < len(data) / 5:
             text = 'Лучше потренируйтесь еще и обязательно повторите теорию'
-        elif data_count_app < 8 * len(data) / 10:
+        elif self.data_count_app < 8 * len(data) / 10:
             text = 'Хороший результат, так держать!'
         else:
             text = 'Прекрасный результат!'
@@ -160,14 +113,13 @@ MDRoundFlatButton:
 
     def show_data(self, id_element):
         input_answer = data_button[id_element].text
-        if input_answer == data_answer[id_element] and data_answer_flag[id_element]:
+        if input_answer == data_answer[id_element] and self.data_answer_flag[id_element]:
             print(input_answer, 'YES')
 
             def dialog_answer_good():
-                global data_count_app
                 dialog.dismiss()
-                data_count_app += 1
-                print(data_count_app)
+                self.data_count_app += 1
+                print(self.data_count_app)
 
             dialog = MDDialog(
                 text='Все верно, продолжим дальше!',
@@ -180,7 +132,7 @@ MDRoundFlatButton:
             )
             dialog.open()
 
-        elif data_answer_flag[id_element]:
+        elif self.data_answer_flag[id_element]:
             print(input_answer, 'NO')
             dialog_2 = MDDialog(
                 text=f'Правильный ответ: {data_answer[id_element]}',
@@ -195,7 +147,7 @@ MDRoundFlatButton:
             def dialog_review():
                 dialog.dismiss()
                 dialog_2.open()
-                data_answer_flag[id_element] = 0
+                self.data_answer_flag[id_element] = 0
 
             dialog = MDDialog(
                 text='Неверно, попробуете заново или посмотреть ответ?',
@@ -226,9 +178,6 @@ MDRoundFlatButton:
     def build(self):
         Window.size = (300, 500)
         return MainScreen()
-
-    def on_start(self):
-        return self.start_app()
 
 
 if __name__ == '__main__':
