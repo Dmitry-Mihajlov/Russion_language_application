3945aff31c1f30c202a2bc9572728b07ca44e4b5
Dmitry-Mihajlov <dimonmihajlov10@gmail.om>
2024-01-22 00:06:12 +0500

    nuw data collection

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import Screen
 from kivy.lang import Builder
 from kivymd.app import MDApp
-from Russion_language_application.data import data, data_answer
+from Russion_language_application.data import data, data_answer, data_word
+import random
 
 Builder.load_file(r'D:\Programming\PyCharm\Russion_language_application\main_screen.kv')
 data_button = []
@@ -41,6 +42,37 @@ class MainApp(MDApp):
         self.data_answer_flag = [1 for i in range(10)]
         self.data_count_app = 0
 
+        for i in range(5):
+            id_word = random.randint(0, 350)
+            text = data_word[f'word_{id_word}']
+            self.locale_1 = f'''
+MDTextField:
+    id: word_{id_word}
+    hint_text: "Ответ"
+    font_style: 'Caption'
+    font_size: '10sp'
+    helper_text_mode: "on_focus"
+'''
+            self.locale_2 = f'''  
+MDRectangleFlatIconButton:
+    icon: 'key'
+    text: 'Проверить ответ'
+    theme_text_color: 'Hint'
+    on_release: app.show_data({i})
+'''
+
+            # добавить текст задания на экран
+            self.copy_label = CopyLabel_1(text=text)
+            self.root.ids.box.add_widget(self.copy_label)
+
+            # добавить кнопки на экран
+            self.data_1 = Builder.load_string(self.locale_1)
+            data_button.append(self.data_1)
+            self.data_2 = Builder.load_string(self.locale_2)
+
+            self.root.ids.box.add_widget(self.data_1)
+            self.root.ids.box.add_widget(self.data_2)
+
         for i in range(len(data)):
             text_1 = data[i][0]
             text_2 = data[i][1]
@@ -60,11 +92,13 @@ MDRectangleFlatIconButton:
     on_release: app.show_data({i})
 '''
 
+            # добавить текст задания на экран
             self.copy_label_1 = CopyLabel_1(text=text_1)
             self.copy_label_2 = CopyLabel_2(text=text_2)
             self.root.ids.box.add_widget(self.copy_label_1)
             self.root.ids.box.add_widget(self.copy_label_2)
 
+            # добавить кнопки на экран
             self.data_1 = Builder.load_string(self.locale_1)
             data_button.append(self.data_1)
             self.data_2 = Builder.load_string(self.locale_2)
