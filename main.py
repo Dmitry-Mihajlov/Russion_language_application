26716ee3e30625fbd03f0a71832861cde88d079f
Dmitry-Mihajlov <dimonmihajlov10@gmail.om>
2024-01-21 12:51:47 +0500

    v4.5

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.textfield import MDTextField
 from data import data, data_answer
-
-Builder.load_file('main_screen.kv')
+import random
 
 data_button = []
+data_count_app = 0
 
 
 class CopyLabel_1(MDLabel):
@@ -39,11 +39,64 @@ class MainScreen(Screen):
 
 class MainApp(MDApp):
 
-    def on_start(self):
-        for i in range(len(data)):
+    def start_app(self):
+        kv = '''
+    <MainScreen>:
+
+    BoxLayout:
+        orientation: 'vertical'
+        MDBottomNavigation:
+            #panel_color: "#eeeaea"
+            text_color_active: "lightgrey"
+            text_color_normal: 1, 1, 1, 1
+            text_color_active: 0, 0, 0, 1
+
+            MDBottomNavigationItem:
+                name: 'screen 1'
+                text: 'Практикум'
+                icon: 'pencil'
+
+                MDBoxLayout:
+                    orientation: "vertical"
+                    MDBoxLayout:
+                        ScrollView:
+                            MDBoxLayout:
+                                id: box
+                                orientation: "vertical"
+                                padding: "10dp"
+                                spacing: "10dp"
+                                adaptive_height: True
+                                MDLabel:
+                                    adaptive_height: True
+                                    font_style: 'Button'
+                                    text: '9 задание'
+                                    halign: 'center'
+
+            MDBottomNavigationItem:
+                name: 'screen 2'
+                text: 'Теория'
+                icon: 'book'
+
+                MDLabel:
+                    text: 'Сакшен Сакшен Сакшен Сакс'
+                    halign: "center"
+
+        '''
+        global data_answer_flag, data_count_app
+        data_count_app = 0
+        data_answer_flag = [1 for i in range(10)]
+
+        Builder.load_string(kv)
+        print('перезапуск')
+        MainApp().start_func(kv)
+
+
+    def start_func(self, kv):
+        data_app = random.choices(data, k=1)
+        for i in range(len(data_app)):
             text_1 = data[i][0]
             text_2 = data[i][1]
-            self.locale_1 = f'''
+            locale_1 = f'''
 MDTextField:
     id: {i}
     hint_text: "Ответ"
@@ -51,7 +104,7 @@ MDTextField:
     font_size: '10sp'
     helper_text_mode: "on_focus"
 '''
-            self.locale_2 = f'''  
+            locale_2 = f'''  
 MDRectangleFlatIconButton:
     icon: 'key'
     text: 'Проверить ответ'
@@ -60,68 +113,122 @@ MDRectangleFlatIconButton:
     
 '''
 
+            copy_label_1 = CopyLabel_1(text=text_1)
+            copy_label_2 = CopyLabel_2(text=text_2)
+            self.root.ids.box.add_widget(copy_label_1)
+            self.root.ids.box.add_widget(copy_label_2)
 
-            self.copy_label_1 = CopyLabel_1(text=text_1)
-            self.copy_label_2 = CopyLabel_2(text=text_2)
-            self.root.ids.box.add_widget(self.copy_label_1)
-            self.root.ids.box.add_widget(self.copy_label_2)
+            data_1 = Builder.load_string(locale_1)
+            data_button.append(data_1)
+            data_2 = Builder.load_string(locale_2)
 
-            self.data_1 = Builder.load_string(self.locale_1)
-            data_button.append(self.data_1)
-            self.data_2 = Builder.load_string(self.locale_2)
+            self.root.ids.box.add_widget(data_1)
+            self.root.ids.box.add_widget(data_2)
 
-            self.root.ids.box.add_widget(self.data_1)
-            self.root.ids.box.add_widget(self.data_2)
+            if i == len(data_app) - 1:
+                self.root.ids.box.add_widget(Builder.load_string(
+                    '''
+MDRoundFlatButton:
+    text: 'Результаты'
+    theme_text_color: 'Hint'
+    on_release: app.result()
+    pos_hint: {"center_x": .5}
+'''
+                ))
 
-    def show_data(self, id):
-        input_answer = data_button[id].text
-        if input_answer == data_answer[id]:
+    def result(self):
+        def func():
+            dialog.dismiss()
+            self.on_start()
+
+        if data_count_app < len(data) / 5:
+            text = 'Лучше потренируйтесь еще и обязательно повторите теорию'
+        elif data_count_app < 8 * len(data) / 10:
+            text = 'Хороший результат, так держать!'
+        else:
+            text = 'Прекрасный результат!'
+        dialog = MDDialog(
+            text=text,
+            buttons=[
+                MDFlatButton(
+                    text='Пройти следующий тест',
+                    on_release=lambda _: func()
+                )
+            ]
+        )
+        dialog.open()
+
+    def show_data(self, id_element):
+        input_answer = data_button[id_element].text
+        if input_answer == data_answer[id_element] and data_answer_flag[id_element]:
             print(input_answer, 'YES')
+
+            def dialog_answer_good():
+                global data_count_app
+                dialog.dismiss()
+                data_count_app += 1
+                print(data_count_app)
 
             dialog = MDDialog(
                 text='Все верно, продолжим дальше!',
                 buttons=[
                     MDFlatButton(
                         text='Следующее задание',
-                        on_release=lambda _: dialog.dismiss()
+                        on_release=lambda _: dialog_answer_good()
                     )
                 ]
             )
-
             dialog.open()
 
-        else:
+        elif data_answer_flag[id_element]:
             print(input_answer, 'NO')
-            self.dialog_2 = MDDialog(
-                text=f'Правильный ответ: {data_answer[id]}',
+            dialog_2 = MDDialog(
+                text=f'Правильный ответ: {data_answer[id_element]}',
                 buttons=[
                     MDFlatButton(
                         text='Закрыть',
-                        on_release=lambda _: self.dialog_2.dismiss()
+                        on_release=lambda _: dialog_2.dismiss()
                     )
                 ]
             )
-            def dialog_aaa():
-                self.dialog.dismiss()
-                self.dialog_2.open()
-            self.dialog = MDDialog(
+
+            def dialog_review():
+                dialog.dismiss()
+                dialog_2.open()
+                data_answer_flag[id_element] = 0
+
+            dialog = MDDialog(
                 text='Неверно, попробуете заново или посмотреть ответ?',
                 buttons=[
                     MDFlatButton(
                         text='Попытаться',
-                        on_release=lambda _: self.dialog.dismiss()
+                        on_release=lambda _: dialog.dismiss()
                     ),
                     MDFlatButton(
                         text='Показать ответ',
-                        on_release=lambda _: dialog_aaa()
+                        on_release=lambda _: dialog_review()
                     )
                 ]
             )
-            self.dialog.open()
+            dialog.open()
+        else:
+            dialog = MDDialog(
+                text='Вы уже посмотрели ответ, поэтому не можете больше вносить изменения',
+                buttons=[
+                    MDFlatButton(
+                        text='Закрыть',
+                        on_release=lambda _: dialog.dismiss()
+                    )
+                ]
+            )
+            dialog.open()
 
     def build(self):
         Window.size = (300, 500)
         return MainScreen()
+
+    def on_start(self):
+        return self.start_app()
 
 
 if __name__ == '__main__':
